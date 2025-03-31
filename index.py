c85fc4d893b735d96d3aefe89c28b4fca79d270f
TKcodes-bit <t.odongo@alustudent.com>
2025-04-01 00:51:01 +0300

    debbuged

--- index.py
+++ index.py
@@ -1088,3 +1088,4 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         print(f"\n{Fore.GREEN}Thank you for using our system. Goodbye!{Style.RESET_ALL}")
+#Run the file
